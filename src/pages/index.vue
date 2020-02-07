dcb4b6557e4af900a33373ea58322ee4d60414cc
GuoJint <1298779616@qq.com>
2020-02-07 16:48:01 +0800

    servicebar组件

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="index">
-        index
+        <service-bar></service-bar>
     </div>
 </template>
 
 <script>
+import ServiceBar from '../components/ServiceBar'
 export default {
     name: 'index',
+    components:{
+        ServiceBar
+    },
     data() { 
         return {
 
